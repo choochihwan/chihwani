26b51cacf2ff8e3cc16d2f85e82fcdcac5d24c59
choochihwan <99579361+choochihwan@users.noreply.github.com>
2022-12-28 01:36:07 +0900

    로그인 화면 마무리

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:chihwani/common/component/custom_text_form_field.dart';
+import 'package:chihwani/user/view/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,12 +14,11 @@ class _App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          
-        ),
-        ),
+      home: LoginScreen(),
     );
   }
 }
